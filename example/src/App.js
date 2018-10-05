fedf8e4305018ed887ca00e5f927a0e0edf67528
Tyler Haas <haasdevelopment@gmail.com>
2018-10-05 15:10:25 -0600

    added ability to take color prop to El component

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import ExampleComponent from 'react-layout-ui'
+import ExampleComponent from "react-layout-ui";
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent color="green" />
       </div>
-    )
+    );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,33 @@
 /**
- * @class ExampleComponent
+ * @class El
  */
 
-import * as React from 'react'
+import * as React from "react";
+import styled from "react-emotion";
+import { StyledStatelessComponent } from "create-emotion-styled";
 
-import styles from './styles.css'
+type Props = {
+  className: string;
+  color: string;
+};
 
-export type Props = { text: string }
+const El: React.SFC<Props> = props => {
+  if (
+    (props && props.children && Array.isArray(props.children)) ||
+    props.children === undefined
+  )
+    throw new Error(
+      `El can only take exactly one child but you are passing ${
+        props.children ? props.children.length : 0
+      } to it.`
+    );
+  return <div className={props.className}>{props.children}</div>;
+};
 
-export default class ExampleComponent extends React.Component<Props> {
-  render() {
-    const {
-      text
-    } = this.props
+const styledEl: StyledStatelessComponent<Props, {}, {}> = styled(El)<Props>(
+  props => ({
+    color: props.color,
+  })
+);
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
-}
+export default styledEl;
